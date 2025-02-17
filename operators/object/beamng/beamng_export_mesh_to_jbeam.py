c8e22d54f9081cd3e610a7575bfb95fb6e0e5c5d
Harry McKenzie <harry.kunz@ymail.com>
2025-02-17 23:14:11 +0800

    add data indented properly with reference to the key prop

--- operators/object/beamng/beamng_export_mesh_to_jbeam.py
+++ operators/object/beamng/beamng_export_mesh_to_jbeam.py
@@ -8,6 +8,7 @@ import io
 class JBeamProcessor:
     def __init__(self, json_data):
         self.json_data = json_data
+        self.input_stream = None
         self.output_stream = io.StringIO()
         self.modified_data = json_data
 
@@ -56,10 +57,27 @@ class JBeamProcessor:
             if not skipping:
                 self.output_stream.write(ch)
 
+    def get_key_indent(self, key):
+        current_pos = self.input_stream.tell()
+        self.input_stream.seek(0)
+
+        for line in self.input_stream:
+            stripped_line = line.lstrip()
+            if stripped_line.startswith(f'"{key}"'):
+                indent = len(line) - len(stripped_line)
+                self.input_stream.seek(current_pos)
+                return indent
+
+        self.input_stream.seek(current_pos) 
+        return -1
+
     def insert_node_contents(self, key, new_contents):
         self.remove_node_contents(key)
+        spaces = self.get_key_indent(key)
+        indent = " " * spaces
         result = self.get_result()
-        result = result.replace(f'"{key}": []', f'"{key}": [\n\t{new_contents}\n]')
+        indented_contents = "\n".join(indent + line for line in new_contents.splitlines())
+        result = result.replace(f'"{key}": []', f'"{key}": [\n\t{indented_contents}\n{indent}]')
         self.modified_data = result
         return result
 

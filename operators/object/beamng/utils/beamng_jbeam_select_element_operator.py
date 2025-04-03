a06f17379ddb27428fcc3681cfa4cf650d2c755a
Harry McKenzie <harry.kunz@ymail.com>
2025-04-03 15:33:47 +0800

    refactor search operator

--- operators/object/beamng/utils/beamng_jbeam_select_element_operator.py
+++ operators/object/beamng/utils/beamng_jbeam_select_element_operator.py
@@ -1,9 +1,8 @@
 import bpy
 import bmesh
 import re
-
 from dev_tools.utils.object_utils import ObjectUtils as o  # type: ignore
-from dev_tools.utils.jbeam.jbeam_utils import JbeamUtils as j # type: ignore
+from dev_tools.utils.jbeam.jbeam_utils import JbeamUtils as j  # type: ignore
 
 class OBJECT_OT_BeamngJbeamSelectSpecificElement(bpy.types.Operator):
     bl_idname = "object.devtools_beamng_jbeam_select_element_by_id_or_index"
@@ -13,7 +12,6 @@ class OBJECT_OT_BeamngJbeamSelectSpecificElement(bpy.types.Operator):
 
     element_id: bpy.props.StringProperty(name="Element Index", default="")  # type: ignore
     element_index: bpy.props.IntProperty(name="Element Index", default=-1)  # type: ignore
-
     is_shift_held: bpy.props.BoolProperty(default=False)  # type: ignore
 
     def invoke(self, context, event):
@@ -28,7 +26,8 @@ class OBJECT_OT_BeamngJbeamSelectSpecificElement(bpy.types.Operator):
         bm = bmesh.from_edit_mesh(obj.data)
         search_by_index = not self.element_id or self.is_shift_held
 
-        def get_element_indices(getter_func, nodes_str, element_type):
+        def get_element_indices(getter_func, element_type, nodes_str):
+            """Helper function to get element indices or handle index-based search."""
             if search_by_index:
                 return [self.element_index]
             indices = getter_func()
@@ -37,10 +36,9 @@ class OBJECT_OT_BeamngJbeamSelectSpecificElement(bpy.types.Operator):
             return indices
 
         def parse_node_ids(node_string):
-            node_string = node_string.strip('[]')
-            node_string = node_string.replace(';', ',').replace('|', ',')  # Convert all delimiters to commas, Replace all delimiters (comma, semicolon, pipe) with a common delimiter (e.g., comma)
-            node_ids = re.findall(r'n\d+', node_string)
-            return node_ids
+            """Parse and format node IDs."""
+            node_string = node_string.strip('[]').replace(';', ',').replace('|', ',')
+            return re.findall(r'n\d+', node_string)
 
         # Deselect all elements
         for v in bm.verts:
@@ -51,15 +49,17 @@ class OBJECT_OT_BeamngJbeamSelectSpecificElement(bpy.types.Operator):
             f.select = False
 
         elements = []
+        element_type = None
         nodes_str = None
+        indices = []
+
         if o.is_vertex_selection_mode():
             bm.verts.ensure_lookup_table()
             element_type = "Node"
             nodes_str = j.get_node_id(obj, self.element_index) if search_by_index else self.element_id
-            indices = get_element_indices(lambda: j.get_node_indices(obj, self.element_id), nodes_str, element_type)
-            for i in indices:
-                if 0 <= i < len(bm.verts):
-                    elements.append(bm.verts[i])
+            indices = get_element_indices(lambda: j.get_node_indices(obj, self.element_id), element_type, nodes_str)
+            elements = [bm.verts[i] for i in indices if 0 <= i < len(bm.verts)]
+
         elif o.is_edge_selection_mode():
             bm.edges.ensure_lookup_table()
             element_type = "Beam"
@@ -67,18 +67,10 @@ class OBJECT_OT_BeamngJbeamSelectSpecificElement(bpy.types.Operator):
             if len(node_ids) != 2:
                 self.report({'WARNING'}, f"{element_type} '{self.element_id}' not found")
                 return {'CANCELLED'}
-            if search_by_index:
-                if 0 <= self.element_index < len(bm.edges):
-                    nodes_str = j.get_beam_id(obj, self.element_index)
-                else:
-                    self.report({'WARNING'}, f"Selected (Index={self.element_index} is out of range!)")
-                    return {'CANCELLED'}
-            else:
-                nodes_str = j.format_node_ids(*node_ids)
-            indices = get_element_indices(lambda: j.get_beam_indices(obj, *node_ids, bm), nodes_str, element_type)
-            for i in indices:
-                if 0 <= i < len(bm.edges):
-                    elements.append(bm.edges[i])
+            nodes_str = j.get_beam_id(obj, self.element_index) if search_by_index and 0 <= self.element_index < len(bm.edges) else j.format_node_ids(*node_ids)
+            indices = get_element_indices(lambda: j.get_beam_indices(obj, *node_ids, bm), element_type, nodes_str)
+            elements = [bm.edges[i] for i in indices if 0 <= i < len(bm.edges)]
+
         elif o.is_face_selection_mode():
             bm.faces.ensure_lookup_table()
             element_type = "Face"
@@ -86,18 +78,10 @@ class OBJECT_OT_BeamngJbeamSelectSpecificElement(bpy.types.Operator):
             if len(node_ids) != 3:
                 self.report({'WARNING'}, f"{element_type} '{self.element_id}' not found")
                 return {'CANCELLED'}
-            if search_by_index:
-                if 0 <= self.element_index < len(bm.faces):
-                    nodes_str = j.get_triangle_id(obj, self.element_index)
-                else:
-                    self.report({'WARNING'}, f"Selected (Index={self.element_index} is out of range!)")
-                    return {'CANCELLED'}
-            else:
-                nodes_str = j.format_node_ids(*node_ids)
-            indices = get_element_indices(lambda: j.get_triangle_indices(obj, *node_ids, bm), nodes_str, element_type)
-            for i in indices:
-                if 0 <= i < len(bm.faces):
-                    elements.append(bm.faces[i])
+            nodes_str = j.get_triangle_id(obj, self.element_index) if search_by_index and 0 <= self.element_index < len(bm.faces) else j.format_node_ids(*node_ids)
+            indices = get_element_indices(lambda: j.get_triangle_indices(obj, *node_ids, bm), element_type, nodes_str)
+            elements = [bm.faces[i] for i in indices if 0 <= i < len(bm.faces)]
+
         if elements:
             for element in elements:
                 element.select = True

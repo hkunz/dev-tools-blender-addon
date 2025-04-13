c65f87a08fc38cc5f48ea89528775453e833e0f2
Harry McKenzie <harry.kunz@ymail.com>
2025-04-13 20:14:41 +0800

    refactor import jbeam operator

--- operators/file/beamng/beamng_import_jbeam_as_node_mesh.py
+++ operators/file/beamng/beamng_import_jbeam_as_node_mesh.py
@@ -28,67 +28,78 @@ class DEVTOOLS_JBEAMEDITOR_IMPORT_OT_BeamngImportJbeamToNodeMesh(Operator, Impor
 
     def execute(self, context):
         bpy.ops.object.select_all(action='DESELECT')
-        jbeam_path = self.filepath
-        filename = os.path.basename(jbeam_path)
+        self.jbeam_path = self.filepath
+        self.filename = os.path.basename(self.jbeam_path)
         self.parser = JbeamParser()
-        fix_required = False
-        cancel = False
-        try:
-            self.parser.load_jbeam(jbeam_path)
-        except Exception as e:
-            json_str = self.parser.get_json_str()
-            error_text = JbeamFileHelper.extract_json_error_snippet(e, json_str)
-            show_warning = a.is_addon_option_enabled("show_import_warnings")
-            Utils.log_and_report(f"Initial load failed with '{e}' Error Text: {error_text}. Trying to auto-fix commas and attempt reload...", self if show_warning else None, "WARNING")
-            with open(jbeam_path, "r", encoding="utf-8") as f:
-                raw = f.read()
-            fixed = JbeamFileHelper.attempt_fix_jbeam_commas(raw)
-            tmp_dir = TempFileManager().create_temp_dir()
-            os.makedirs(tmp_dir, exist_ok=True)
-            file_path1 = os.path.join(tmp_dir, filename)
-            file_path2 = os.path.join(tmp_dir, f"{filename}.json")
-            try:
-                fix_required = True
-                self.parser.load_jbeam_from_string(fixed)
-                print(f"Auto-Fix and Load Success {filename}")
-            except Exception as e2:
-                error_text = JbeamFileHelper.extract_json_error_snippet(e2, json_str)
-                Utils.log_and_report(f"Failed to fix and load file: '{e2}' Error Text: {error_text}\nWrote attempted fix file to: {file_path1}", self, "ERROR")
-                cancel = True
-            try:
-                json_str = self.parser.get_json_str()
-                with open(file_path1, 'w') as f:
-                    f.write(fixed)
-                with open(file_path2, 'w') as f:
-                    f.write(json_str)
-            except Exception as write_error:
-                Utils.log_and_report(f"Failed to write the attempted fix file: {write_error}", self, "ERROR")
-
-        if cancel:
+        success = self.load_jbeam_file_and_regiter_parts()
+        if not success:
             return {'CANCELLED'}
-
-        jbeam_parts: dict[str, object] = self.parser.get_jbeam_parts()
-
-        for part_name, part in jbeam_parts.items():
-            print(f"Creating Part with name '{part_name}' ================================>")
-            nodes_list = self.parser.get_nodes_list(part_name)
-            if not nodes_list:
-                Utils.log_and_report(f"No nodes list in part name '{part_name}'", self, "INFO")
-                continue
-            mesh_name = f"{os.path.splitext(filename)[0]}.{part_name}"
-            jmc = JbeamNodeMeshCreator()
-            obj = jmc.create_object(mesh_name)
-            jmc.add_vertices(nodes_list)
-            self.parser.parse_data_for_jbeam_object_conversion(obj, part_name, False)
-
-            beams_list = self.parser.get_beams_list(part_name)
-            tris_list = self.parser.get_triangles_list(part_name)
-            jmc.add_edges(beams_list)
-            jmc.add_faces(tris_list)
-
-            JbeamNodeMeshConfigurator.process_node_mesh_props(obj, self.parser, part_name)
-            obj.select_set(True)
-            bpy.context.view_layer.objects.active = obj
-        info = f"Auto-Fix and Import Success" if fix_required else f"Import Success"
-        Utils.log_and_report(f"{info}: {filename}", self, "INFO")
+        self.create_node_meshes()
+        Utils.log_and_report(f"Import Success: {self.filename}", self, "INFO")
         return {'FINISHED'}
+
+    def load_jbeam_file_and_regiter_parts(self):
+        success = True
+        try:
+            self.parser.load_jbeam(self.jbeam_path)
+        except Exception as e:
+            jbeam_fixed_str = self.get_jbeam_file_as_fixed_string(e)
+            success = self.load_jbeam_string_and_regiter_parts(jbeam_fixed_str)
+        return success
+
+    def load_jbeam_string_and_regiter_parts(self, jbeam_str):
+        tmp_dir = TempFileManager().create_temp_dir()
+        os.makedirs(tmp_dir, exist_ok=True)
+        file_path1 = os.path.join(tmp_dir, self.filename)
+        file_path2 = os.path.join(tmp_dir, f"{self.filename}.json")
+        try:
+            self.parser.load_jbeam_from_string(jbeam_str)
+            print(f"Auto-Fix and Load Success {self.filename}")
+        except Exception as e2:
+            error_text = JbeamFileHelper.extract_json_error_snippet(e2, json_str)
+            Utils.log_and_report(f"Failed to fix and load file: '{e2}' Error Text: {error_text}\nWrote attempted fix file to: {file_path1}", self, "ERROR")
+            return False
+        try:
+            json_str = self.parser.get_json_str()
+            with open(file_path1, 'w') as f:
+                f.write(jbeam_str)
+            with open(file_path2, 'w') as f:
+                f.write(json_str)
+        except Exception as write_error:
+            Utils.log_and_report(f"Failed to write the attempted fix file: {write_error}", self, "ERROR")
+        return True
+
+    def get_jbeam_file_as_fixed_string(self, e):
+        json_str = self.parser.get_json_str()
+        error_text = JbeamFileHelper.extract_json_error_snippet(e, json_str)
+        show_warning = a.is_addon_option_enabled("show_import_warnings")
+        Utils.log_and_report(f"Initial load failed with '{e}' Error Text: {error_text}. Trying to auto-fix commas and attempt reload...", self if show_warning else None, "WARNING")
+        with open(self.jbeam_path, "r", encoding="utf-8") as f:
+            raw = f.read()
+        return JbeamFileHelper.attempt_fix_jbeam_commas(raw)
+
+    def create_node_meshes(self):
+        jbeam_parts: dict[str, object] = self.parser.get_jbeam_parts()
+        for part_name, part in jbeam_parts.items():
+            self.create_node_mesh(part_name)
+
+    def create_node_mesh(self, part_name):
+        print(f"Creating Part with name '{part_name}' ================================>")
+        nodes_list = self.parser.get_nodes_list(part_name)
+        if not nodes_list:
+            Utils.log_and_report(f"No nodes list in part name '{part_name}'", self, "INFO")
+            return
+        mesh_name = f"{os.path.splitext(self.filename)[0]}.{part_name}"
+        jmc = JbeamNodeMeshCreator()
+        obj = jmc.create_object(mesh_name)
+        jmc.add_vertices(nodes_list)
+        self.parser.parse_data_for_jbeam_object_conversion(obj, part_name, False)
+
+        beams_list = self.parser.get_beams_list(part_name)
+        tris_list = self.parser.get_triangles_list(part_name)
+        jmc.add_edges(beams_list)
+        jmc.add_faces(tris_list)
+
+        JbeamNodeMeshConfigurator.process_node_mesh_props(obj, self.parser, part_name)
+        obj.select_set(True)
+        bpy.context.view_layer.objects.active = obj
